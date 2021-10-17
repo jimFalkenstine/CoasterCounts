df2106ec155b3f15b0c088db5f92a53206dc9be7
jimFalkenstine <jim.falkenstine@gmail.com>
2021-10-17 13:48:14 -0400

    Added DDL to create CoasterCounts database and Coasters, Parks, & Riders tables.

--- CoasterCounts.sql
+++ CoasterCounts.sql
@@ -1,2 +1,31 @@
 CREATE DATABASE CoasterCounts;
 
+CREATE TABLE Coasters (
+	CoasterID INT IDENTITY PRIMARY KEY,
+	CoasterName VARCHAR(255),
+	CoasterPark VARCHAR(255),
+	CoasterType VARCHAR(255),
+	CoasterDesign VARCHAR(255),
+	CoasterMake VARCHAR(255),
+	CoasterLength INT,
+	CoasterHeight INT,
+	CoasterInversions INT,
+	CoasterYear INT,
+	CoasterStatus VARCHAR(255),
+);
+
+CREATE TABLE Parks (
+	ParkID INT IDENTITY PRIMARY KEY,
+	ParkName VARCHAR(255),
+	ParkCity VARCHAR(255),
+	ParkState VARCHAR(255),
+	ParkCountry VARCHAR(255),
+	OpeningDate DATE 
+);
+
+CREATE TABLE Riders (
+	RiderID INT IDENTITY PRIMARY KEY,
+	RiderName VARCHAR(255),
+	RiderBirthdate DATE
+);
+
