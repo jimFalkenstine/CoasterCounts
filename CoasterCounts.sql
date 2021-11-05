6824b79861d0e3c2532bbde75c8233677fa82ad3
jimFalkenstine <jim.falkenstine@gmail.com>
2021-11-05 10:27:51 -0400

    Fixed spacing issue

--- CoasterCounts.sql
+++ CoasterCounts.sql
@@ -3,8 +3,7 @@
 */
 
 /** Add a Rider with the name of AF and birthdate of 03/08/1983 **/
-EXECUTE AddRider @RiderName = 'AF',
-				 @RiderBirthdate = '1983-03-08';
+EXECUTE AddRider @RiderName = 'AF', @RiderBirthdate = '1983-03-08';
 
 
 /** Get a list of all of the Coasters ridden by the Rider with a RiderID of 1 **/
@@ -16,5 +15,4 @@ EXECUTE UpdateCoasterStatus @CoasterID = 14;
 
 
 /** Delete the Coaster with a CoasterID of 1 from the Coaster Count of the Rider with a RiderID of 1 **/
-EXECUTE DeleteCoasterFromCount @RiderID = 1,
-							   @CoasterID = 1;
+EXECUTE DeleteCoasterFromCount @RiderID = 1, @CoasterID = 1;
