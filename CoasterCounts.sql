92ebe96125f8735118629bc54b6c0994015cc559
jimFalkenstine <jim.falkenstine@gmail.com>
2021-11-08 23:48:53 -0500

    Updated sample commands for added 'Read' stored procedures.

--- CoasterCounts.sql
+++ CoasterCounts.sql
@@ -6,12 +6,20 @@
 EXECUTE AddRider @RiderName = 'AF', @RiderBirthdate = '1983-03-08';
 
 
-/** Get a list of all of the Coasters ridden by the Rider with a RiderID of 1 **/
+/**Get a list of all Coasters at Kings Island arranged alphabetically **/
+EXECUTE ReadCoastersByPark @ParkName = 'Kings Island';
+
+
+/**Get a list of all Coasters Manufactured by Bolliger & Mabillard arranged alphabetically by Park then by Coaster **/
+EXECUTE ReadCoastersByManufacturer @ManufacturerName = 'Bolliger & Mabillard';
+
+
+/** Get a list of all of the Coasters ridden by the Rider with a RiderID of 1 arranged alphabetically by Park then by Coaster **/
 EXECUTE ReadCoasterCount @RiderID = 1;
 
 
 /** Update the status of the Coaster with a CoasterID of 14 to 'Removed' **/
-EXECUTE UpdateCoasterStatus @CoasterID = 14;
+EXECUTE UpdateCoasterStatus @CoasterID = 14, @CoasterStatus = 'Removed';
 
 
 /** Delete the Coaster with a CoasterID of 1 from the Coaster Count of the Rider with a RiderID of 1 **/
